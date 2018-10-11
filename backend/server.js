0b5729e06353080bdd68a607c23b3c5e458368bf
danielivanov <daniel.k.ivanov95@gmail.com>
2018-10-11 16:48:03 +0300

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,76 +1,67 @@
 'use strict';
 const express = require('express');
 const axios = require('axios');
-const jsonWallet = require('./wallet');
+const jsonWallet = "Shopper's JSON Wallet";
 const app = express();
 
+const LIME_PAY_BASE_URL;
 
-// // Created in the Remote Test MongoDB
-// const VENDOR_ID = "5ba3826bc190b1bc2362d2b7";
-// const SHOPPER_ID = "5ba385a0c190b1bc2362d2c7";
+const VENDOR_ID = "YOUR_VENDOR_ID_HERE";
+const SHOPPER_ID = "SHOPPERS_ID_HERE";
 
-// const API_KEY = "1888c350bcc711e898ec3dfe32153918";
-// const API_SECRET = "be26192f19a05d1101569b7454d7ea88eb78d1b48c54ee7d9bcbc18da010ee7416d60ff9769e27cc836bd6e5e6ac4f5693412133bf62782039abc1199cc46bb5a13a994efbbd3bd5b762bdae8aa9fe2c3a041c8b7fcfda94f4c0e540f78bd326c4489ad89be7cc646308871f5b9be52406cd370285238c6bf3da842a7c9e8858";
-// Created in the Remote Test MongoDB
-const VENDOR_ID = "5bb35907cafe6fcc4dd16ef0";
-const SHOPPER_ID = "5bb35916cafe6fcc4dd16ef3";
-
-const API_KEY = "dd0826c0c63711e8a28e3fa29e9358da";
-const API_SECRET = "e0eb86ffeb73c25f985ea2fe7a606476af124511fa0c8d906fefdc2f9331e44c6c89b2bc2c26b9b6d0cf5f460a8d3f54c649334b204518de57a54bd7a8519918c27dbfe064dd14c0dc677fb8f0391cbba1706629bad5116f6ee1f7ff10c27b2a7d1294da88f61775169e811be9c268695ece16cd45026eaf01c753f74cb5f121";
-const URL = "http://localhost:3000/v1/payments"
+const API_KEY = "YOUR_API_KEY_HERE";
+const API_SECRET = "YOUR_SECRET_HERE";
+const CREATE_PAYMENT_URL = LIME_PAY_BASE_URL + "/v1/payments"
 
 
 app.use('/static', express.static('public'));
+
 app.get('/', async (req, res, next) => {
     try {
         // Get LimePay Token and return it to the UI
 
+        let paymentData = {
+            "currency": "USD",
+            "shopper": SHOPPER_ID,
+            "vendor": VENDOR_ID,
+            "items": [
+                {
+                    "description": "my crypto apple",
+                    "amount": 100,
+                    "quantity": 1
+                }
+            ],
+            "fundTxData": {
+                "tokenAmount": "10000000000000000000",
+                "weiAmount": "60000000000000000"
+            },
+            "genericTransactions": [
+                {
+                    "to": "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
+                    "gasPrice": "18800000000",
+                    "gasLimit": "4700000",
+                    "functionName": "approve"
+                },
+                {
+                    "to": "0x07F3fB05d8b7aF49450ee675A26A01592F922734",
+                    "gasPrice": "18800000000",
+                    "gasLimit": "4700000",
+                    "functionName": "buySomeService"
+                }
+            ]
+        }
+
         let result = await axios({
             method: "POST",
-            url: URL,
+            url: CREATE_PAYMENT_URL,
             headers: {
                 "Content-Type": "application/json",
                 "Cache-Control": "no-cache",
                 "Authorization": "Basic " + Buffer.from(API_KEY + ":" + API_SECRET).toString('base64')
             },
-            data: {
-                "currency": "USD",
-                "shopper": SHOPPER_ID,
-                "vendor": VENDOR_ID,
-                "items": [
-                    {
-                        "description": "Негърч1е",
-                        "amount": 100.4,
-                        "quantity": 1
-                    },
-                    {
-                        "description": "Death Nigga2",
-                        "amount": 23.2,
-                        "quantity": 2
-                    }
-                ],
-                "fundTxData": {
-                    "tokenAmount": "10000000000000000000",
-                    "weiAmount": "60000000000000000"
-                },
-                "genericTransactions": [
-                    {
-                        "gasPrice": "18800000000",
-                        "gasLimit": "4700000",
-                        "contractAddress": "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
-                        "spender": "0x1835f2716ba8f3ede4180c88286b27f070efe985",
-                        "functionName": "transfer"
-                    },
-                    {
-                        "gasPrice": "18800000000",
-                        "gasLimit": "4700000",
-                        "contractAddress": "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
-                        "spender": "0x1835f2716ba8f3ede4180c88286b27f070efe985",
-                        "functionName": "transfer"
-                    }
-                ]
-            }
+            data: paymentData
         });
+
         let token = result.headers["x-lime-token"];
         res.json({ token: token, jsonWallet: jsonWallet });
     } catch (err) {
@@ -84,5 +75,5 @@ app.use((err, request, response, next) => {
 });
 
 var server = app.listen(9090, () => {
-    console.log("Marketplace app listening at http://localhost:" + 9090);
+    console.log("Sample APP listening at http://localhost:" + 9090);
 });
