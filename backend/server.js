6430ac6bcc4ca46dbf4280647c98a80946a6b619
danielivanov <daniel.k.ivanov95@gmail.com>
2018-10-15 13:41:54 +0300

    update to new changes

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,6 @@ const axios = require('axios');
 const jsonWallet = require('./wallet');
 const app = express();
 
-
-// // Created in the Remote Test MongoDB
-// const VENDOR_ID = "5ba3826bc190b1bc2362d2b7";
-// const SHOPPER_ID = "5ba385a0c190b1bc2362d2c7";
-
-// const API_KEY = "1888c350bcc711e898ec3dfe32153918";
-// const API_SECRET = "be26192f19a05d1101569b7454d7ea88eb78d1b48c54ee7d9bcbc18da010ee7416d60ff9769e27cc836bd6e5e6ac4f5693412133bf62782039abc1199cc46bb5a13a994efbbd3bd5b762bdae8aa9fe2c3a041c8b7fcfda94f4c0e540f78bd326c4489ad89be7cc646308871f5b9be52406cd370285238c6bf3da842a7c9e8858";
-// Created in the Remote Test MongoDB
-const VENDOR_ID = "5bb35907cafe6fcc4dd16ef0";
 const SHOPPER_ID = "5bb35916cafe6fcc4dd16ef3";
 
 const API_KEY = "dd0826c0c63711e8a28e3fa29e9358da";
@@ -36,7 +27,6 @@ app.get('/', async (req, res, next) => {
             data: {
                 "currency": "USD",
                 "shopper": SHOPPER_ID,
-                "vendor": VENDOR_ID,
                 "items": [
                     {
                         "description": "Негърч1е",
@@ -57,15 +47,13 @@ app.get('/', async (req, res, next) => {
                     {
                         "gasPrice": "18800000000",
                         "gasLimit": "4700000",
-                        "contractAddress": "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
-                        "spender": "0x1835f2716ba8f3ede4180c88286b27f070efe985",
+                        "to": "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
                         "functionName": "transfer"
                     },
                     {
                         "gasPrice": "18800000000",
                         "gasLimit": "4700000",
-                        "contractAddress": "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
-                        "spender": "0x1835f2716ba8f3ede4180c88286b27f070efe985",
+                        "to": "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
                         "functionName": "transfer"
                     }
                 ]
@@ -74,7 +62,8 @@ app.get('/', async (req, res, next) => {
         let token = result.headers["x-lime-token"];
         res.json({ token: token, jsonWallet: jsonWallet });
     } catch (err) {
-        console.log(err.response.data);
+        res.json(err.response.data);
+
     }
 });
 
