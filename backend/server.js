37bb5c32e40fbc9cd8420885f160cda34701ddbc
Daniel Ivanov <daniel@limechain.tech>
2019-01-17 17:40:36 +0200

    save changes

--- backend/server.js
+++ backend/server.js
@@ -1,139 +1,134 @@
 'use strict';
 const express = require('express');
 const axios = require('axios');
-const jsonWallet = require('./wallet');
+const LimePaySDK = require('limepay');
+let LimePay;
+
+const shopperWallet = require('./shopper-wallet');
+const signerWallet = require('./signer-wallet');
+const CONFIG = require('./config');
+
+
+const signerWalletConfig = {
+    encryptedWallet: {
+        jsonWallet: JSON.stringify(signerWallet),
+        password: CONFIG.SIGNER_WALLET_PASSPHRASE
+    }
+};
+
 const app = express();
 
-const CONFIG = require('./../config/config');
-const HOST = CONFIG.HOST;
-const APP_CREDENTIALS = CONFIG.APP_CREDENTIALS;
+// Connect to LimePay API
 
-// organization = 5be1b8ba9cb8aa22efadc827
-const API_KEY = APP_CREDENTIALS.API_KEY;
-const API_SECRET = APP_CREDENTIALS.API_SECRET;
-const SHOPPER_ID = CONFIG.SHOPPER_ID;
+app.use('/', express.static('public'));
 
-async function getLimeToken(url, data) {
-    // Get LimePay Token and return it to the UI
-    let result = await axios({
-        method: "POST",
-        url: url,
-        headers: {
-            "Content-Type": "application/json",
-            "Cache-Control": "no-cache",
-            "Authorization": "Basic " + Buffer.from(API_KEY + ":" + API_SECRET).toString('base64')
-        },
-        data: data
-    });
-
-    let token = result.headers["x-lime-token"];
-    return token;
-}
-
-app.use('/static', express.static('public'));
-
-app.get('/', async (req, res, next) => {
-    const URL = HOST + "/v1/payments"
+app.post('/fiatPayment', async (request, response, next) => {
     try {
-
-        let fiatData = {
-            "currency": "USD",
-            "shopper": SHOPPER_ID,
-            "items": [{
-                    "description": "Some good description",
-                    "lineAmount": 100.4,
-                    "quantity": 1
-                },
-                {
-                    "description": "Another description",
-                    "lineAmount": 25.2,
-                    "quantity": 2
-                }
-            ],
-            "fundTxData": {
-                "tokenAmount": "10000000000000000000",
-                "weiAmount": "60000000000000000"
-            },
-            "genericTransactions": [{
-                "gasPrice": "18800000000",
-                "gasLimit": "4700000",
-                "to": "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
-                "functionName": "transfer",
-                "functionParams": [{
-                        type: 'address',
-                        value: "0x1835f2716ba8f3ede4180c88286b27f070efe985",
-                    },
-                    {
-                        type: 'uint',
-                        value: 0,
-                    }
-                ]
-            }]
-        }
-
-        let token = await getLimeToken(URL, fiatData);
-
-        res.json({
-            token: token
-        });
-    } catch (err) {
-        console.log('ERROR');
-        console.log(err.response ? err.response.data : err);
-
-        res.json(err.response ? err.response.data : err);
+        const fiatPaymentData = getFiatData();
+        fiatPaymentData.shopper = "5c3e090cfd77ee6054c03883";  // Hard-coded shopper ID
+        
+        const createdPayment = await LimePay.fiatPayment.create(fiatPaymentData, signerWalletConfig);
+        response.json({ token: createdPayment.limeToken });
+    } catch (error) {
+        next(error);
     }
+
 });
 
-app.get('/relayed', async (req, res, next) => {
-    const URL = HOST + "/v1/payments/relayed";
+app.post('/relayedPayment', async (request, response, next) => {
     try {
-
-        let relayedData = {
-            "shopper": SHOPPER_ID,
-            "fundTxData": {
-                "weiAmount": "60000000000000000"
-            },
-            "genericTransactions": [{
-                "gasPrice": "18800000000",
-                "gasLimit": "4700000",
-                "to": "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
-                "functionName": "transfer",
-                "functionParams": [{
-                        type: 'address',
-                        value: "0x1835f2716ba8f3ede4180c88286b27f070efe985",
-                    },
-                    {
-                        type: 'uint',
-                        value: 0,
-                    }
-                ]
-            }]
-        }
-
-        let token = await getLimeToken(URL, relayedData);
-
-        res.json({
-            token: token
-        });
-    } catch (err) {
-        console.log('ERROR');
-        console.log(err.response ? err.response.data : err);
-
-        res.json(err.response ? err.response.data : err);
+        const relayedPaymentData = getRelayedData();
+        relayedPaymentData.shopper = "5c3e090cfd77ee6054c03883";  // Hard-coded shopper ID
+    
+        const createdPayment = await LimePay.relayedPayment.create(relayedPaymentData, signerWalletConfig);
+        response.json({ token: createdPayment.limeToken });
+    } catch (error) {
+        next(error)
     }
+
 });
 
-app.get('/wallet', async (req, res, next) => {
-    res.json({
-        jsonWallet: JSON.stringify(jsonWallet)
-    });
+app.get('/shopperWallet', async (req, res, next) => {
+    res.json(shopperWallet);
 });
 
 app.use((err, request, response, next) => {
     console.log(err);
-    response.status(500).send("Something went wrong.");
+    response.status(500).send(err);
 });
 
-var server = app.listen(9090, () => {
-    console.log(`Sample app listening at http://localhost:` + 9090);
+app.listen(9090, async () => {
+    LimePay = await LimePaySDK.connect({
+        environment: 'http://localhost:3000/v1', // LimePay.Environment.Sandbox.Default,
+        apiKey: CONFIG.API_KEY,
+        secret: CONFIG.API_SECRET
+    });
+
+    console.log(`Sample app listening at http://localhost:` + 9090)
 });
+
+const getFiatData = () => {
+    return {
+        currency: "USD",
+        items: [
+            {
+                description: "Some good description",
+                lineAmount: 100.4,
+                quantity: 1
+            },
+            {
+                description: "Another description",
+                lineAmount: 25.2,
+                quantity: 2
+            }
+        ],
+        fundTxData: {
+            tokenAmount: "10000000000000000000",
+            weiAmount: "60000000000000000"
+        },
+        genericTransactions: [
+            {
+                gasPrice: "18800000000",
+                gasLimit: "4700000",
+                to: "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
+                functionName: "transfer",
+                functionParams: [
+                    {
+                        type: 'address',
+                        value: "0x1835f2716ba8f3ede4180c88286b27f070efe985",
+                    },
+                    {
+                        type: 'uint',
+                        value: 0,
+                    }
+                ]
+            }
+        ]
+    };
+};
+
+const getRelayedData = () => {
+    return {
+        fundTxData: {
+            weiAmount: "60000000000000000"
+        },
+        genericTransactions: [
+            {
+                gasPrice: "18800000000",
+                gasLimit: "4700000",
+                to: "0xc8b06aA70161810e00bFd283eDc68B1df1082301",
+                functionName: "transfer",
+                functionParams: [
+                    {
+                        type: 'address',
+                        value: "0x1835f2716ba8f3ede4180c88286b27f070efe985",
+                    },
+                    {
+                        type: 'uint',
+                        value: 0,
+                    }
+                ]
+            }
+        ]
+    }
+}
---
+++ public/scripts/script.js
@@ -0,0 +1,148 @@
+let processFiatPayment;
+let processRelayedPayment;
+let fiatPayment;
+let relayedPayment;
+let limePayConfig;
+let tokenABI;
+
+const SHOPPER_WALLET_PASSPHRASE = "some cool passphrase";
+
+window.onload = async function () {
+    (function onWindowLoad() {
+        $.getJSON('./../constants/countries-codes.json', function (countriesCodes) {
+            $.each(countriesCodes, function (code) {
+                let dropdownCountryCode = `<option value="${code}">${code} (${countriesCodes[code]})</option>`;
+                $(dropdownCountryCode).appendTo("#countries-codes");
+            });
+        });
+
+        $.getJSON('./../constants/TokenABI.json', function (abi) {
+            tokenABI = abi;
+        });
+        
+        limePayConfig = {
+            environment: 'http://localhost:3000',
+            eventHandler: {
+                onSuccessfulSubmit: function () {
+                    alert('Your payment was send for processing');
+                    processAnimation.stopProcessingAnimation();
+                    // Implement some logic
+                },
+                onFailedSubmit: function (err) {
+                    console.log(err);
+                    alert('Your payment failed');
+                    processAnimation.stopProcessingAnimation();
+                    // Implement some logic
+                }
+            }
+        }
+    })();
+
+    processAnimation.init();
+
+    $('input[type=radio][name=initialization]').change(async function() {
+        if (this.value == 'fiat') {
+            await initialiseFiatPayment();
+        } else if (this.value == 'relayed') {
+            await initialiseRelayedPayment();
+        }
+    });
+
+    async function initialiseFiatPayment () {
+        // Make backend call for the creation of the payment
+        const result = await $.post('/fiatPayment');
+        
+        // Unlocks the payment form
+        fiatPayment = await LimePayWeb.initFiatPayment(result.token, limePayConfig);
+    }
+
+    async function initialiseRelayedPayment() {
+        // Make backend call for the creation of the payment
+        const result = await $.post('/relayedPayment');
+
+        relayedPayment = await LimePayWeb.initRelayedPayment(result.token, limePayConfig);
+    }
+
+    // The function is trigger once the user submits the payment form
+    processFiatPayment = async function () {
+        
+        // Get the shopper JSON Wallet from the backend
+        const shopperWallet = await $.get('/shopperWallet');
+        
+        const walletConfiguration = {
+            encryptedWallet: {
+                jsonWallet: JSON.stringify(shopperWallet),
+                password: SHOPPER_WALLET_PASSPHRASE
+            }
+        };
+        const transactions = await getTransactions();
+
+        // Signs the provided transactions using the Shoppers wallet
+        await fiatPayment.buildSignedTransactions(walletConfiguration, transactions);
+
+        // Extracting the Card holder information from the form
+        const cardHolderInformation = {
+            vatNumber: document.getElementById('vat-number').value,
+            name: document.getElementById('card-holder-name').value,
+            countryCode: document.getElementById('countries-codes').value,
+            zip: document.getElementById('zip-code').value,
+            street: document.getElementById('street-address').value
+        };
+
+        if (document.getElementById('company').checked) {
+            cardHolderInformation.isCompany = true;
+        } else if (document.getElementById('personal').checked) {
+            cardHolderInformation.isCompany = false;
+        }
+
+        // Triggers the processing of the payment
+        await fiatPayment.processPayment(cardHolderInformation);
+    }
+
+    processRelayedPayment = async function () {
+        processAnimation.startProcessingAnimation();
+
+        // Get the shopper JSON Wallet from the backend
+        const shopperWallet = await $.get('/shopperWallet');
+
+        const walletConfiguration = {
+            encryptedWallet: {
+                jsonWallet: JSON.stringify(shopperWallet),
+                password: SHOPPER_WALLET_PASSPHRASE
+            }
+        };
+        const transactions = await getTransactions();
+
+        // Signs the provided transactions using the Shoppers wallet
+        await relayedPayment.buildSignedTransactions(walletConfiguration, transactions);
+
+        // Triggers the processing of the payment
+        await relayedPayment.processRelayedPayment();
+    }
+
+    async function getTransactions() {
+        return [
+            {
+                to: '0x30D25785515bE27d0B46Ab41Ed57dBAbf8A9cFf6',
+                abi: tokenABI,
+                gasLimit: 4700000,
+                value: 0,
+                functionName: "approve",
+                functionParams: ["0x37688cFc875DC6AA6D39fE8449A759e434a86482", '10000000000000000000'] // Approve '0x37688cFc875DC6AA6D39fE8449A759e434a86482' to spend 10 Tokens 
+            },
+            {
+                to: '0x37688cFc875DC6AA6D39fE8449A759e434a86482',
+                abi: tokenABI, // the ABI of your contract here
+                gasLimit: 4700000,
+                value: 0, // Put the amount of ethers if required
+                functionName: "buySomeService",
+                functionParams: [] // Put the parameters of the 'buySomeService' function here
+            }
+        ];
+    }
+    
+    onInvalidCompanyField = function () {
+        processAnimation.stopProcessingAnimation();
+    }
+}
+
